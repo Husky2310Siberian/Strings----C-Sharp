26989235796ad57c1c69b7535f0493dea11132ab
Siberian <vasileios.kritikos@aueb.gr>
2025-03-15 14:09:31 +0200

    Regular expression

--- NullableStrings/Program.cs
+++ NullableStrings/Program.cs
@@ -1,9 +1,34 @@
-﻿namespace NullableStrings;
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace NullableStrings;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string s1;  //not nullable
+        string? s2; //nullable
+        
+        Console.WriteLine("Please insert s1 and s2");
+        s1 = Console.ReadLine()!;
+        s2 = Console.ReadLine();
+
+        if (s2 != null)
+        {
+            if (s1.Equals("Hello") && s2.Equals("World!!!"))
+            {
+                
+            }
+        }
+    }
+
+    public static string? GetUpperCaseString()
+    {
+        string? s1;
+        Console.WriteLine("Please insert uppercase string");
+        s1 = Console.ReadLine();
+        
+        return s1?.ToUpper();
+        
     }
 }
---
+++ RegEx/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegEx;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string input = "hello123";
+        string pattern = @"[a-z]+\d";
+        
+        OneMatch(input, pattern);
+    }
+
+    public static void OneMatch(string input, string pattern)
+    {
+        Match match = Regex.Match(input, pattern);
+        if (match.Success)
+        {
+            Console.WriteLine(match.Value);
+        }
+    }
+}
