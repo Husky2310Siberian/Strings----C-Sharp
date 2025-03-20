3031f9028c7284dc88f53d8d15ebb4026022479e
Siberian <vasileios.kritikos@aueb.gr>
2025-03-20 11:05:43 +0200

    Slow/Fast concat

---
+++ FastNumbersConcat/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace FastNumbersConcat;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(DateTime.Now);
+        System.Text.StringBuilder sb = new System.Text.StringBuilder();
+        sb.Append("Numbers: ");
+
+        for (int i = 1; i <= 5000; i++)
+        {
+            sb.Append(i);
+        }
+        Console.WriteLine(sb.ToString().Substring(0, 1024));
+        Console.WriteLine(DateTime.Now);
+        
+        
+    }
+}
---
+++ SlowNumbersConcat/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace SlowNumbersConcat;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(DateTime.Now);
+        string collector = "Numbers: ";
+
+        for (int i = 1; i <= 5000; i++)
+        {
+            collector += i;
+        }
+        Console.WriteLine(collector.Substring(0,1024));
+        Console.WriteLine(DateTime.Now);
+        
+        
+    }
+}
